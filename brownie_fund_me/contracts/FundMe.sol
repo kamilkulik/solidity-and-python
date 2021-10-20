ef77bf45e65632474e77b1256539183045705cf2
kamilkulikTeck <kulikkamil@gmail.com>
2021-10-20 07:35:07 +0200

    parametrize fundMe contract with contructor variable priceFeed

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -7,71 +7,49 @@ import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
     using SafeMathChainlink for uint256;
-
+    
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
     address public owner;
     AggregatorV3Interface public priceFeed;
     
-    // if you're following along with the freecodecamp video
-    // Please see https://github.com/PatrickAlphaC/fund_me
-    // to get the starting solidity contract code, it'll be slightly different than this!
     constructor(address _priceFeed) public {
         priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
-
+    
     function fund() public payable {
-        uint256 mimimumUSD = 50 * 10**18;
-        require(
-            getConversionRate(msg.value) >= mimimumUSD,
-            "You need to spend more ETH!"
-        );
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
-
-    function getVersion() public view returns (uint256) {
+    
+    function getVersion() public view returns (uint256){
         return priceFeed.version();
     }
-
-    function getPrice() public view returns (uint256) {
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
-        return uint256(answer * 10000000000);
+    
+    function getPrice() public view returns(uint256){
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+         return uint256(answer * 10000000000);
     }
-
+    
     // 1000000000
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
     }
-
-    function getEntranceFee() public view returns (uint256) {
-        // mimimumUSD
-        uint256 mimimumUSD = 50 * 10**18;
-        uint256 price = getPrice();
-        uint256 precision = 1 * 10**18;
-        return (mimimumUSD * precision) / price;
-    }
-
-    modifier onlyOwner() {
+    
+    modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-
-    function withdraw() public payable onlyOwner {
+    
+    function withdraw() payable onlyOwner public {
         msg.sender.transfer(address(this).balance);
-
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
+        
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
