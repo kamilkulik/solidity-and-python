c8b410eed504df54b7b1afb0df4201ca9b8feec4
kamilkulikTeck <kulikkamil@gmail.com>
2021-10-24 22:17:05 +0200

    small fix

--- brownie_fund_me/scripts/utils.py
+++ brownie_fund_me/scripts/utils.py
@@ -2,12 +2,13 @@ from brownie import accounts, config, network, MockV3Aggregator
 from web3 import Web3
 
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+STARTING_PRICE = 200000000000
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
 
@@ -16,7 +17,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks deployed")
