d84c9e5ebe0fdddc366972deb83e765e39f20187
kamilkulikTeck <kulikkamil@gmail.com>
2021-10-24 22:18:14 +0200

    small fix in deploy script

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import config, FundMe, MockV3Aggregator, network
-from scripts.utils import deploy_mocks, get_account
+from scripts.utils import deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 
 def deploy_fund_me():
@@ -8,7 +8,7 @@ def deploy_fund_me():
     # becase it is used in the contract's constructor
     # if we are on a persistent network like rinkeby, use associated address
     # otherwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
     else:
         deploy_mocks()
